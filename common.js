4d9e9a85a0e36d5a1c1bcad8156ba7665cbf7854
chanchi- <chanchi0707@gmail.com>
2016-09-08 00:51:12 +0900

    ランダムにユーザー表示

--- common.js
+++ common.js
@@ -2,9 +2,45 @@
 var mySwipe = angular.module("mySwipe",["ngResource"]);
 
 	mySwipe.controller("MainCtrl",["$scope","$resource",function($scope,$resource){
+
+		//jsonをngに格納
 		var contents = $resource("user.json");
 		$scope.users = contents.query();
+
+		//ランダムに表示
+		$scope.random = function(){
+			return 0.5 - Math.random();
+		};
+
+		var rd = $scope.random;
+
+
+
+		//入れる場所の用意
+		$scope.likeusers = [];
+		$scope.nopeusers = [];
+
+
+		//likeの場合
+		$scope.like = function(){
+			$scope.likeusers.push({
+				firstname : $scope.users[0].firstname,
+				lastname : $scope.users[0].lastname,
+				image : $scope.users[0].images,
+			});
+		};
+
+		//nopeの場合
+		$scope.nope = function(){
+			$scope.nopeusers.push({
+				firstname : $scope.users[0].firstname,
+				lastname : $scope.users[0].lastname,
+				image : $scope.users[0].images,
+			});
+		};
 	}]);
+
+
 
 
 
